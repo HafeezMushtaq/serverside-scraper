fc37667f16d527e3bed1ee68df633676945c6c50
hafeez.mushtaq <hafeez.mushtaq@autotrader.co.uk>
2019-01-16 22:46:44 +0000

    Implement the converter

--- src/main/java/uk/co/sainsbury/service/ProductInformationConverter.java
+++ src/main/java/uk/co/sainsbury/service/ProductInformationConverter.java
@@ -1,13 +1,28 @@
 package uk.co.sainsbury.service;
 
+import uk.co.sainsbury.model.DisplayProduct;
 import uk.co.sainsbury.model.ProductInformation;
+import uk.co.sainsbury.usecase.Total;
 import uk.co.sainsbury.utility.Product;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProductInformationConverter {
 
     public ProductInformation convert(List<Product> products) {
-        return new ProductInformation(null, null);
+        List<DisplayProduct> displayProducts = products.stream()
+                .map(product -> new DisplayProduct(product.getTitle(), product.getEnergy(), product.getPrice(), product.getDescription()))
+                .collect(Collectors.toList());
+
+        BigDecimal gross = products.stream()
+                .map(Product::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal grossWithoutVat = gross.divide(new BigDecimal("1.20"), 2, BigDecimal.ROUND_HALF_UP);
+        BigDecimal vat = gross.subtract(grossWithoutVat);
+
+        return new ProductInformation(displayProducts, new Total(gross, vat));
     }
 }
--- src/test/java/uk/co/sainsbury/service/ProductInformationConverterTest.java
+++ src/test/java/uk/co/sainsbury/service/ProductInformationConverterTest.java
@@ -1,7 +1,53 @@
 package uk.co.sainsbury.service;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import uk.co.sainsbury.model.DisplayProduct;
+import uk.co.sainsbury.model.ProductInformation;
+import uk.co.sainsbury.utility.Product;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class ProductInformationConverterTest {
 
+    private ProductInformationConverter converter = new ProductInformationConverter();
+
+    @Test
+    public void convert_shouldConvertTheProductIntoDisplayProductOnProductInfo() {
+        Product product = new Product("Kiwi", 325, BigDecimal.TEN, "Healthy Stuff");
+        DisplayProduct displayProduct = new DisplayProduct("Kiwi", 325, BigDecimal.TEN, "Healthy Stuff");
+
+        ProductInformation result = converter.convert(Collections.singletonList(product));
+
+        assertThat(result.getProducts()).containsOnly(displayProduct);
+    }
+
+    @Test
+    public void convert_shouldConvertTheProductsIntoDisplayProductsOnProductInfo() {
+        Product product = new Product("Kiwi", 325, BigDecimal.TEN, "Healthy Stuff");
+        Product product2 = new Product("Banana", 650, BigDecimal.ONE, "Awesome");
+        DisplayProduct displayProduct = new DisplayProduct("Kiwi", 325, BigDecimal.TEN, "Healthy Stuff");
+        DisplayProduct displayProduct2 = new DisplayProduct("Banana", 650, BigDecimal.ONE, "Awesome");
+
+        ProductInformation result = converter.convert(Arrays.asList(product, product2));
+
+        assertThat(result.getProducts()).contains(displayProduct, displayProduct2);
+    }
+
+    @Test
+    public void convert_shouldCalculateTheCorrectProductTotals() {
+        Product product = new Product("Kiwi", 325, new BigDecimal("1.25"), "Healthy Stuff");
+        Product product2 = new Product("Banana", 650, new BigDecimal("2.55"), "Awesome");
+        DisplayProduct displayProduct = new DisplayProduct("Kiwi", 325, new BigDecimal("1.25"), "Healthy Stuff");
+        DisplayProduct displayProduct2 = new DisplayProduct("Banana", 650, new BigDecimal("2.55"), "Awesome");
+
+        ProductInformation result = converter.convert(Arrays.asList(product, product2));
+
+        assertThat(result.getProducts()).contains(displayProduct, displayProduct2);
+        assertThat(result.getTotal().getGross()).isEqualTo("3.80");
+        assertThat(result.getTotal().getVat()).isEqualTo("0.63");
+    }
 }
